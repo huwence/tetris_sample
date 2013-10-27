45ed24478b5303dc69ef1a9edf2221ac2f2a37b9
huwence <huwence@gmail.com>
2013-10-27 22:43:53 +0800

    Updated logics for tetris game

--- tetris.js
+++ tetris.js
@@ -1,99 +1,236 @@
-function Block(x, y, width, height, color, flag) {
-    this.x = x;
-    this.y = y;
-    this.width  = width;
-    this.height = height;
-    this.color = color || '#ccc';
-    this.flag = flag;
-}
-
-Block.prototype.draw(ctx) {
-    ctx.fillStyle = this.color;
-    ctx.fillRect(this.x, this,y, this.width, this.height);
-}
-
-var WIDTH = 10,
+var WIDTH = 20,
     HEIGHT = WIDTH,
     CANVAS_WIDTH = 320,
-    CANVAS_HEIGHT = 550,
+    CANVAS_HEIGHT = 400,
     LEN = 3,
     NUM = 4,
-    START_X = CANVAS_WIDTH / 2 - WIDTH / 2,
+    FLOORS = CANVAS_HEIGHT / HEIGHT - 1,
+    COUNTS = CANVAS_WIDTH / WIDTH - 1, 
+    START_X = CANVAS_WIDTH / 2 - WIDTH,
     START_Y = 0,
-    VELOCITY_Y = 5, 
-    PREFIX = 'COMPOSITE_';
+    VELOCITY_Y = 2, 
+    GRID = {},
     COMPOSITE = { 
-        COMPOSITE_1: [[[1, 0], [1, 1], [0, 1]]], //Sqaure
-        COMPOSITE_2: [[[0, -1], [0, 1], [0, 2]], [[-1, 0], [1, 0], [2, 0]]], //Rect
-        COMPOSITE_3: [[[0, -1], [1, 0], [-1, 0]], [[0, -1], [1, 0], [0, 1]], [[1, 0], [0, 1], [-1, 0]], [[0, -1], [0, 1], [-1, 0]]],
-        COMPOSITE_4: [[[1, 0], [0, -1], [0, -2]], [[1, 0], [2, 0], [0, 1]], [[0, 1], [0, 2], [-1, 0]], [[0, -1], [-1, 0], [-2, 0]]]
+        COMPOSITE_1: [[[-1, -1], [0, -1], [-1, 0]]], //Sqaure
+        COMPOSITE_2: [[[0, -1], [0, -2], [0, -3]], [[1, 0], [2, 0], [3, 0]]], //Rect
+        COMPOSITE_3: [[[0, -1], [1, 0], [-1, 0]], [[0, -1], [0, -2], [1, -1]], [[-1, -1], [0, -1], [1, -1]], [[0, -2], [0, -1], [-1, -1]]],
+        COMPOSITE_4: [[[0, -1], [0, -2], [1, 0]], [[0, -1], [1, -1], [2, -1]], [[0, -1], [0, -2], [-1, -2]], [[0, -1], [-1, 0], [-2, 0]]]
     },
-    BLOCKS = [];
+    BLOCKS = [],
+    BLOCKS_HASH = {},
+    FLOOR_MIN = null,
+    CONTEXT = null,
+    PREFIX = 'COMPOSITE_',
+    FLOOR_PREFIX = 'FLOOR_',
+    IS_END = false
 
-//type = 1, 2, 3, 4
-function updateComposite(blocks) {
-    if (!blocks.length)
-        return;
-
-    var block_start = blocks[i];
-
-    block_start.draw(CONTEXT);
-    for (var i = 1; i < LEN + 1; i ++) {
-        block.x = block_start.x + composite[i][0] * WIDTH;
-        block.y = block_start.y + composite[i][1] * HEIGHT;
-
-        block.draw(CONTEXT);
-    }
-
+function Block(guid, x, y, width, height, color) {
+    this.guid = guid
+    this.x = x
+    this.y = y
+    this.width  = width
+    this.height = height
+    this.color = color || '#ccc'
 }
 
-function updateBlock(block) {
-    if (!block)
-        return;
+Block.prototype.draw = function (ctx) {
+    ctx.fillStyle = this.color
+    ctx.fillRect(this.x, this.y, this.width, this.height)
+}
 
-    block.draw(CONTEXT);
+function getGuid() {
+    var now = +new Date(),
+        random = Math.random().toString().split('.')[1] + ''
+
+    return Number(random.substring(16) + now).toString(32)
 }
 
 function generateComposite() {
+    if (IS_END)
+        return
+
     var type = Math.ceil(Math.random() * 4),
-        composites = COMPOSITE[PREFIX + type];
+        composites = COMPOSITE[PREFIX + type]
 
     if (!composites)
-        return;
+        return
 
-    var composite = composites[0],
-        block_start = new Block(START_X, START_Y, WIDTH, HEIGHT, 1);
+    var shape = Math.round(Math.random() * (composites.length - 1)),
+        composite = composites[shape],
+        block_header_guid = getGuid(),
+        block_header = new Block(block_header_guid, START_X, START_Y, WIDTH, HEIGHT, 'blue')
+        block_header.feature = [type, shape]
 
-    BLOCKS.push(block_start);
+    BLOCKS.push(block_header)
+    BLOCKS_HASH[block_header_guid] = block_header
 
     for (var i = 0; i < LEN; i ++) {
-        BLOCKS.push(new Block(START_X + composite[i][0] * WIDTH, START_Y + composite[i][1] * HEIGHT, WIDTH, HEIGHT));
+        var guid = getGuid(),
+            block = new Block(guid, START_X + composite[i][0] * WIDTH, START_Y + composite[i][1] * HEIGHT, WIDTH, HEIGHT) 
+
+        BLOCKS.push(block)
+        BLOCKS_HASH[guid] = block
+    }
+
+}
+
+function updateBlockHeader(block_header) {
+    var x = block_header.x,
+        y = block_header.y + VELOCITY_Y,
+        floor = Math.floor((y + HEIGHT) / HEIGHT),
+        index = Math.floor(x / WIDTH),
+        grid, 
+        is_collision = false
+
+    if (CANVAS_HEIGHT - HEIGHT < y) {
+        delete block_header.feature
+
+        y = CANVAS_HEIGHT - HEIGHT
+        is_collision = true
+        generateComposite()
+    }
+
+    grid = GRID[FLOOR_PREFIX + floor]
+
+    if (grid && grid.items[index]) {
+        delete block_header.feature
+
+        generateComposite()
+        x = index * WIDTH 
+        y = (floor - 1) * HEIGHT
+        is_collision = true
+
+        if (0 === floor - 1) {
+            IS_END = true
+        }
+    }
+
+    block_header.x = x
+    block_header.y = y
+
+    return is_collision
+}
+
+//type = 1, 2, 3, 4
+function updateComposite(blocks, feature) {
+    if (!blocks.length)
+        return
+
+    var is_collision = updateBlockHeader(blocks[0]),
+        composite = COMPOSITE[PREFIX + feature[0]][feature[1]],
+        floor, index
+
+    if (IS_END)
+        return
+
+    for (var i = 0, l = blocks.length; i < l; i ++) {
+
+        if (i != 0) {
+            blocks[i].x = blocks[0].x + composite[i - 1][0] * WIDTH
+            blocks[i].y = blocks[0].y + composite[i - 1][1] * HEIGHT
+        }
+
+        if (is_collision) {
+            index = Math.floor(blocks[i].x / WIDTH)
+            floor = Math.floor(blocks[i].y  / HEIGHT)
+            con = GRID[FLOOR_PREFIX + floor]
+
+            con.items[index] = blocks[i].guid
+            con.num += 1
+        }
+
+        blocks[i].draw(CONTEXT)
     }
 }
 
-function update() {
+function updateGrid(blocks) {
+    FLOOR_MIN = null
 
+    for (var i = 0, l = blocks.length; i < l; i ++) {
+        var floor = Math.floor(blocks[i].y / HEIGHT),
+            index = Math.floor(blocks[i].x / WIDTH),
+            content = GRID[FLOOR_PREFIX + floor]
+
+        content.items[index] = blocks[i].guid
+        content.num += 1
+
+        if (content.num === COUNTS) {
+            for (var j = 0, k = content.items.length; j < k; j ++) {
+                FLOOR_MIN = Math.min(FLOOR_MIN, floor)
+                BLOCKS_HASH[content.items[j]].flag = 1
+            }
+        }
+    }
+
+}
+
+function update() {
     if (BLOCKS.length === 0) {
-        generateComposite();
+        generateComposite()
     }
 
     for (var i = 0, l = BLOCKS.length; i < l; i ++) {
         var block = BLOCKS[i],
-            flag  = block.flag;
+            floor = block.y % HEIGHT
+            flag = block.flag,
+            feature = block.feature
 
-        //composite
         if (flag) {
-            i += NUM;
-            block.y += VELOCITY_Y;
-            updateComposite([block, BLOCKS[i + 1], BLOCKS[i + 2], BLOCKS[i + 3]]);
+            BLOCKS.splice(i, 1)
+            -- l
+            continue
+        }
+
+        if (FLOOR_MIN && floor < FLOOR_MIN) {
+            block.y += HEIGHT
+        }
+
+        if (feature) {
+            updateComposite([block, BLOCKS[i + 1], BLOCKS[i + 2], BLOCKS[i + 3]], feature)
+            i += NUM
         } else {
-            updateBlock(block);
+            block.draw(CONTEXT)
         }
     }
 }
-    
 
-function main() {
+function requestFrame(callback) {
+    var enterFrame = window.requestAnimationFrame       ||
+                     window.webkitRequestAnimationFrame ||
+                     window.mozRequestAnimationFrame    ||
+                     window.oRequestAnimationFrame      ||
+                     window.msRequestAnimationFrame     ||
+                     function (callback) {
+                         setTimeout(callback, 1000 / 60)
+                     }
 
-
+    enterFrame(callback)
 }
+
+function render() {
+    requestFrame(function () {
+        CONTEXT.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT)
+        update()
+        render()
+    })
+}
+    
+function main() {
+    var canvas = document.getElementById('stage')
+
+    canvas.width  = CANVAS_WIDTH
+    canvas.height = CANVAS_HEIGHT
+    canvas.style.background = '#000'
+
+    for (var i = 0; i <= FLOORS; i ++) {
+        GRID[FLOOR_PREFIX + i] = {
+            'items': new Array(COUNTS),
+            'num': 0
+        } 
+    }
+
+    CONTEXT = canvas.getContext('2d')
+    render()
+}
+
+main()
